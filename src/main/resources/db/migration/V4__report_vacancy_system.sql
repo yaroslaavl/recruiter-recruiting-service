fb4521cf5b961147416e0857e14bf4728e7a85d4
ylopatkin <ylopatkin@gmail.com>
2025-08-31 14:15:58 +0200

    31.08 - updated migration by adding new fields to entities

--- src/main/resources/db/migration/V4__report_vacancy_system.sql
+++ src/main/resources/db/migration/V4__report_vacancy_system.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS recruiting_data.report_system (
                                                            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                                                            vacancy_id UUID NOT NULL REFERENCES recruiting_data.vacancy(id) ON DELETE CASCADE,
-                                                           user_id UUID NOT NULL,
+                                                           user_id VARCHAR(255) NOT NULL,
                                                            reason VARCHAR(50) NOT NULL,
                                                            status VARCHAR(50) NOT NULL DEFAULT 'NEW',
                                                            comment TEXT,
---
+++ src/main/resources/db/migration/V5__change_field_type.sql
@@ -0,0 +1,2 @@
+ALTER TABLE recruiting_data.application_history
+ALTER COLUMN changed_by TYPE varchar(255);
