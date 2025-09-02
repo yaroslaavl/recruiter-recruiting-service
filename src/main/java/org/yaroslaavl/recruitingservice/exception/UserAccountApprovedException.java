2a4fde925a9d3821907f7f90006b1fcd81fad78c
ylopatkin <ylopatkin@gmail.com>
2025-09-02 21:30:25 +0200

    02.09 - added UserAccountApprovedException to handle specific case

---
+++ src/main/java/org/yaroslaavl/recruitingservice/exception/UserAccountApprovedException.java
@@ -0,0 +1,7 @@
+package org.yaroslaavl.recruitingservice.exception;
+
+public class UserAccountApprovedException extends RuntimeException {
+    public UserAccountApprovedException(String message) {
+        super(message);
+    }
+}
