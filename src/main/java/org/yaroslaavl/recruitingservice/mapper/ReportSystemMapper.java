870d2c6aa9e915ace02a22239f2c784d15833907
ylopatkin <ylopatkin@gmail.com>
2025-09-13 21:38:04 +0200

    13.09 - updated mappers

--- src/main/java/org/yaroslaavl/recruitingservice/mapper/ReportSystemMapper.java
+++ src/main/java/org/yaroslaavl/recruitingservice/mapper/ReportSystemMapper.java
@@ -1,14 +1,33 @@
 package org.yaroslaavl.recruitingservice.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.yaroslaavl.recruitingservice.database.entity.ReportSystem;
 import org.yaroslaavl.recruitingservice.dto.response.ReportSystemResponseDto;
-import org.yaroslaavl.recruitingservice.dto.response.ReportSystemShortDto;
+import org.yaroslaavl.recruitingservice.dto.response.list.ReportSystemShortDto;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper(componentModel = "spring")
 public interface ReportSystemMapper {
 
-    ReportSystemShortDto toShortDto(ReportSystem reportSystem);
+    @Mapping(target = "id", source = "reportSystem.id")
+    @Mapping(target = "reportedAt", source = "reportSystem.createdAt")
+    @Mapping(target = "status", source = "reportSystem.status")
+    @Mapping(target = "displayName", expression = "java(mapUserEmail(reportSystem, usersDisplayName))")
+    ReportSystemShortDto toShortDto(ReportSystem reportSystem, Map<String, String> usersDisplayName);
 
+    default List<ReportSystemShortDto> toShortDto(List<ReportSystem> reportSystemList, Map<String, String> usersDisplayName) {
+        return reportSystemList.stream()
+                .map(reportSystem -> toShortDto(reportSystem, usersDisplayName))
+                .toList();
+    }
+
+    @Mapping(target = "reportedAt", source = "createdAt")
     ReportSystemResponseDto toResponseDto(ReportSystem reportSystem);
+
+    default String mapUserEmail(ReportSystem reportSystem, Map<String, String> usersDisplayName) {
+        return usersDisplayName.get(reportSystem.getUserId());
+    }
 }
--- src/main/java/org/yaroslaavl/recruitingservice/mapper/VacancyMapper.java
+++ src/main/java/org/yaroslaavl/recruitingservice/mapper/VacancyMapper.java
@@ -1,14 +1,16 @@
 package org.yaroslaavl.recruitingservice.mapper;
 
-import org.aspectj.lang.annotation.Before;
-import org.mapstruct.BeforeMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 import org.yaroslaavl.recruitingservice.database.entity.Vacancy;
 import org.yaroslaavl.recruitingservice.dto.request.VacancyRequestDto;
 import org.yaroslaavl.recruitingservice.dto.request.VacancyUpdateRequestDto;
 import org.yaroslaavl.recruitingservice.dto.response.VacancyResponseDto;
+import org.yaroslaavl.recruitingservice.dto.response.list.VacancyShortDto;
+import org.yaroslaavl.recruitingservice.feignClient.dto.CompanyPreviewFeignDto;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Mapper(componentModel = "spring")
 public interface VacancyMapper {
@@ -16,7 +18,42 @@ public interface VacancyMapper {
     @Mapping(target = "category.id", source = "categoryId")
     Vacancy toEntity(VacancyRequestDto vacancyRequestDto);
 
-    void updateEntity(VacancyUpdateRequestDto vacancyUpdateRequestDto, Vacancy vacancy);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntity(VacancyUpdateRequestDto vacancyUpdateRequestDto, @MappingTarget Vacancy vacancy);
 
+    @Mapping(target = "category", source = "category.name")
     VacancyResponseDto toDto(Vacancy vacancy);
+
+    @Mapping(target = "category", source = "vacancy.category.name")
+    @Mapping(target = "companyId", expression = "java(getCompanyId(vacancy, previewInfo))")
+    @Mapping(target = "companyName", expression = "java(getCompanyName(vacancy, previewInfo))")
+    @Mapping(target = "companyLogoUrl", expression = "java(getCompanyLogoUrl(vacancy, previewInfo))")
+    @Mapping(target = "companyLocation", expression = "java(getCompanyLocation(vacancy, previewInfo))")
+    VacancyShortDto toShortDto(Vacancy vacancy, Map<UUID, CompanyPreviewFeignDto> previewInfo);
+
+    default List<VacancyShortDto> toShortDto(List<Vacancy> vacancies, Map<UUID, CompanyPreviewFeignDto> previewInfo) {
+        return vacancies.stream()
+                .map(vacancy -> toShortDto(vacancy, previewInfo))
+                .toList();
+    }
+
+    default String getCompanyName(Vacancy vacancy, Map<UUID, CompanyPreviewFeignDto> previewInfo) {
+        CompanyPreviewFeignDto preview = previewInfo.get(vacancy.getCompanyId());
+        return preview != null ? preview.name() : null;
+    }
+
+    default String getCompanyLogoUrl(Vacancy vacancy,  Map<UUID, CompanyPreviewFeignDto> previewInfo) {
+        CompanyPreviewFeignDto preview = previewInfo.get(vacancy.getCompanyId());
+        return preview != null ? preview.logoUrl() : null;
+    }
+
+    default UUID getCompanyId(Vacancy vacancy,  Map<UUID, CompanyPreviewFeignDto> previewInfo) {
+        CompanyPreviewFeignDto preview = previewInfo.get(vacancy.getCompanyId());
+        return preview != null ? preview.id() : null;
+    }
+
+    default String getCompanyLocation(Vacancy vacancy, Map<UUID, CompanyPreviewFeignDto> previewInfo) {
+        CompanyPreviewFeignDto companyPreviewFeignDto = previewInfo.get(vacancy.getCompanyId());
+        return companyPreviewFeignDto != null ? companyPreviewFeignDto.location() : null;
+    }
 }
