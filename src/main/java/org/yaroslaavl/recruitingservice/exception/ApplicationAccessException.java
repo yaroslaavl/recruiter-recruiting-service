5fd6bc55c3486c67ac7f3d10ec3f79c585cb2dd2
ylopatkin <ylopatkin@gmail.com>
2025-09-13 21:37:15 +0200

    13.09 - added new custom exceptions

---
+++ src/main/java/org/yaroslaavl/recruitingservice/exception/ApplicationAccessException.java
@@ -0,0 +1,7 @@
+package org.yaroslaavl.recruitingservice.exception;
+
+public class ApplicationAccessException extends RuntimeException {
+    public ApplicationAccessException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/yaroslaavl/recruitingservice/exception/GetUserInfoException.java
@@ -0,0 +1,7 @@
+package org.yaroslaavl.recruitingservice.exception;
+
+public class GetUserInfoException extends RuntimeException {
+    public GetUserInfoException(String message) {
+        super(message);
+    }
+}
