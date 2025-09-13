ff2c00519068b9f675ea1500a81dd2092ff4ac43
ylopatkin <ylopatkin@gmail.com>
2025-09-13 21:36:41 +0200

    13.09 - added dto for list response

--- src/main/java/org/yaroslaavl/recruitingservice/dto/response/list/ApplicationShortDto.java
+++ src/main/java/org/yaroslaavl/recruitingservice/dto/response/list/ApplicationShortDto.java
@@ -1,4 +1,4 @@
-package org.yaroslaavl.recruitingservice.dto.response;
+package org.yaroslaavl.recruitingservice.dto.response.list;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -7,9 +7,13 @@ import org.yaroslaavl.recruitingservice.database.entity.enums.RecruitingSystemSt
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record ApplicationResponseDto(
+public record ApplicationShortDto(
         @NotNull UUID id,
-        @NotBlank String candidateId,
+        @NotBlank String displayName,
+        String salary,
+        String workMode,
+        Integer availableHoursPerWeek,
+        String availableFrom,
         @NotNull RecruitingSystemStatus status,
         @NotNull LocalDateTime appliedAt
 ) { }
--- src/main/java/org/yaroslaavl/recruitingservice/dto/response/list/ReportSystemShortDto.java
+++ src/main/java/org/yaroslaavl/recruitingservice/dto/response/list/ReportSystemShortDto.java
@@ -1,5 +1,6 @@
-package org.yaroslaavl.recruitingservice.dto.response;
+package org.yaroslaavl.recruitingservice.dto.response.list;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.yaroslaavl.recruitingservice.database.entity.enums.RecruitingSystemStatus;
 
@@ -8,7 +9,7 @@ import java.util.UUID;
 
 public record ReportSystemShortDto(
         @NotNull UUID id,
-        @NotNull String userId,
+        @NotBlank String displayName,
         @NotNull RecruitingSystemStatus status,
         @NotNull LocalDateTime reportedAt
 ) { }
