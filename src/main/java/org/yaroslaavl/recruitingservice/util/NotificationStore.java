89966882ad50e263e114d78ddd3fdcfb65c33b2d
ylopatkin <ylopatkin@gmail.com>
2025-09-18 22:36:33 +0200

    13.09 - added utility for in-app notification creation

---
+++ src/main/java/org/yaroslaavl/recruitingservice/util/NotificationStore.java
@@ -0,0 +1,21 @@
+package org.yaroslaavl.recruitingservice.util;
+
+import lombok.experimental.UtilityClass;
+import org.yaroslaavl.recruitingservice.broker.NotificationDto;
+
+import java.util.Map;
+
+@UtilityClass
+public class NotificationStore {
+
+    public static NotificationDto inAppNotification(String userId, String targetUserId, String entityId, String entityType, Map<String, String> properties) {
+        return NotificationDto.builder()
+                .userId(userId)
+                .targetUserId(targetUserId)
+                .entityId(entityId)
+                .entityType(entityType)
+                .notificationType("DASHBOARD_APP")
+                .contentVariables(properties)
+                .build();
+    }
+}
