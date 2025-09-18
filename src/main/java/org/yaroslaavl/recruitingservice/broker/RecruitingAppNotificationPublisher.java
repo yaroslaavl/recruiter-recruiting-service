0ac4665245f74369134a2925297433ea7e9c3fc1
ylopatkin <ylopatkin@gmail.com>
2025-09-18 22:36:49 +0200

    18.09 - added RabbitMQ configuration and publisher for in-app notifications

---
+++ src/main/java/org/yaroslaavl/recruitingservice/broker/RecruitingAppNotificationPublisher.java
@@ -0,0 +1,26 @@
+package org.yaroslaavl.recruitingservice.broker;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.yaroslaavl.recruitingservice.service.VacancyService;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RecruitingAppNotificationPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.recruiting.exchange}")
+    public String exchange;
+
+    @Value("${rabbitmq.recruiting.queues.start.routing-key}")
+    public String routingKey;
+
+    public void publishInAppNotification(NotificationDto notificationDto) {
+        rabbitTemplate.convertAndSend(exchange, routingKey, notificationDto);
+    }
+}
